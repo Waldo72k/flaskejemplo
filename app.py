c15b0b98d4b31dfc879145db8fc930d00d926620
Waldo <oswaldo.ct18@gmail.com>
2023-02-27 19:33:12 -0700

    Le meti mas para que hiciera una consulta de ejemplo de mi db

--- app.py
+++ app.py
@@ -3,12 +3,31 @@
 #flask --app app run
 
 from flask import Flask
+import mysql.connector
+
+conexion = mysql.connector.connect(
+
+user = "root", password ="fenix7200",
+host="localhost",
+database="videojuegosmodels",
+port="3306",)
+
+print(conexion)
+
+miCursor = conexion.cursor()
+
+miCursor.execute("SELECT * FROM videojuegos WHERE ID = 2")
+
+consulta = miCursor.fetchall()
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "HOLA ITE Ensenada"
+    return "<h1><center>HOLA ITE Ensenada</center></h1><marquee>% s</marquee>"%consulta
 
 @app.route("/bye")
 def adios():
     return "bye bye"
+
+#flask --app app run --port=80 <-asi se arranca el api, desde la virtualizacion
