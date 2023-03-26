dd9f8970233b2373067b8e3757b868e6aae7c6b6
Waldo <oswaldo.ct18@gmail.com>
2023-03-26 16:39:52 -0700

    Agregue endpoints que regresan json con la info de cada uno

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ print(conexion)
 
 miCursor = conexion.cursor()
 
-miCursor.execute("SELECT * FROM videojuegos WHERE ID = 1")
+miCursor.execute("SELECT * FROM videogames WHERE ID = 1")
 
 consulta = miCursor.fetchall()
 
@@ -25,15 +25,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<h1><center>HOLA ITE Ensenada</center></h1>"
-##Asi se declara un pinchi endpoint, en este caso es el /bye
-@app.route("/bye")
-def adios():
-    return "bye bye"
+    return "<h1><center>VGDb</center></h1>"
+##Asi se declara un endpoint, en este caso es el /bye
+@app.route("/videogames")
+def videogames():
+    miCursor.execute("SELECT * FROM videogames")
+    data = []
+    for row in miCursor:
+        data.append(row)
+    return jsonify(data)
+
+@app.route("/developers")
+def developers():
+    miCursor.execute("SELECT * FROM developers")
+    data = []
+    for row in miCursor:
+        data.append(row)
+    return jsonify(data)
+
+@app.route("/publishers")
+def publishers():
+    miCursor.execute("SELECT * FROM publishers")
+    data = []
+    for row in miCursor:
+        data.append(row)
+    return jsonify(data)
 
 @app.route("/sql/test")
 def testConsulta():
-    return "<h1><center>HOLA ITE Ensenada</center></h1><marquee>% s</marquee>"%consulta
+    return "<h1><center>Test de consulta de MySQL</center></h1><marquee>% s</marquee>"%consulta
 
 @app.route("/about")
 def about():
